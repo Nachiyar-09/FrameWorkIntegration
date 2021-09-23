2817b028ee064efd8b12f97b8ad63eee7b985087
Nachiyar-09 <85991689+Nachiyar-09@users.noreply.github.com>
2021-09-23 01:59:26 -0700

    Adding changes related to employee list

--- src/main/java/Com/FrameworkIntegration/Pages/AdminPage.java
+++ src/main/java/Com/FrameworkIntegration/Pages/AdminPage.java
@@ -1,11 +1,19 @@
 package Com.FrameworkIntegration.Pages;
 
+import net.serenitybdd.core.pages.WebElementFacade;
 import net.thucydides.core.annotations.Step;
 import net.thucydides.core.pages.PageObject;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.util.List;
 
 public class AdminPage extends PageObject {
 
+    private PIMPage pimPage;
     private WebDriver driver = getDriver();
     @Step
     public void clickAdmin(){
@@ -22,10 +30,32 @@ public class AdminPage extends PageObject {
         $("#btnAdd").click();
     }
     @Step
-    public void EnterEmployeeName(String EmpName){
-        $("#systemUser_employeeName_empName").type(EmpName);
-    }@Step
-public void EnterUserName (String Username){
+    public void selectUserRole(String role){
+        $("#systemUser_userType").selectByVisibleText(role);
+
+    }
+    @Step
+    public void EnterEmployeeNameAndVerify(String Name,String ErrorMessage) {
+                  //Handling autosuggestion in Employee name
+        $("#systemUser_employeeName_empName").type(Name);
+        if(pimPage.EmployeeTable().contains(Name)){              // verify employee list from PIM module
+             WebDriverWait wait = new WebDriverWait(getDriver(),30);
+             wait.until(ExpectedConditions.visibilityOfAllElementsLocatedBy(By.xpath("/html/body/div[4]/ul")));
+
+             List<WebElement> auto = getDriver().findElements(By.xpath("/html/body/div[4]/ul"));
+
+             for( WebElement list : auto){
+                 System.out.println("Values are = " + list.getText());
+                 if(list.getText().contains(Name))
+                     list.click();
+             }}
+         else{
+             ErrorMessage = getDriver().findElement(By.xpath("//*[@id=\"frmSystemUser\"]/fieldset/ol/li[2]/span")).getText();
+             System.out.println(ErrorMessage);
+
+         }}
+       @Step
+      public void EnterUserName (String Username){
     $("#systemUser_userName").type(Username);
 }
     @Step
@@ -49,3 +79,4 @@ public void EnterUserName (String Username){
         return new PIMPage();
     }
 }
+
